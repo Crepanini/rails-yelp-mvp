63b1a5b6eaa4eab089e10bdfac8e32d1c9087a7a
Nini Zhu <pn.zhu76@gmail.com>
2019-03-27 14:34:17 +0800

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'reviews/new'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
+
+# VISIT
+# - restaurants#index
+# - restuarants#show
+
+# - restaurants#new
+# - restaurants#create
+
+#only admin get to update and destory
+
+
+# - add a new review
+# GET "restaurants/38/reviews/new"
+# POST "restaurants/38/reviews"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.delete_all if Rails.env.development?
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.full_address, phone_number: Faker::PhoneNumber.cell_phone, category: %w[chinese italian japanese french belgian].sample)
+end
+
+puts "You've created #{Restaurant.count} restaurants"
